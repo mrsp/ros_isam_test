dbfe9fa287d5b3602e8b3c9b2040f72aeab3ce3d
mrsp <stpiperakis@gmail.com>
2020-02-22 16:36:02 +0200

    working with landmarks

--- src/isam_ros_test.cpp
+++ src/isam_ros_test.cpp
@@ -16,38 +16,68 @@ kparams_t params;
 
 Isam myIsam(params);
 Affine3d tempPose = Affine3d::Identity();
-Matrix<double,6,6> tempCov = Matrix<double,6,6>::Identity();
+Matrix<double,6,6> tempCov = Matrix<double,6,6>::Identity()*1e-6;
 myIsam.init(tempPose,tempCov);
 int originPoseIdx = 0;
-std::cout<<"Temp Cov"<<tempCov<<std::endl;
 
-tempPose(0,3)=0.79; //True is 0.99
-tempPose(1,3)=1.09; //0.99 
-tempPose(2,3)=0.9; //0.99
 
-tempCov =  Matrix<double,6,6>::Identity() * 1e-1;
+//True Displacement:
+Vector3d trueDist = Vector3d(0.99,0.99,0.99);
+
+
+//First Factor with Noise
+tempPose(0,3)=0.49; // 0.99
+tempPose(1,3)=1.49; //0.99 
+tempPose(2,3)=0.3; //0.99
+tempCov =  Matrix<double,6,6>::Identity() *10;
+//Add Factor to the Graph
 myIsam.addFrame(tempPose, tempCov);
 int firstPoseIdx = 1;
 
-Vector3d landMarkPosFromFirstPose = Vector3d(0,1.00,0);
-Vector3d landMarkPosFromOrigin = Vector3d(-0.99,1.99,-0.99);
-Matrix3d landMarkCov = Matrix3d::Identity()*1e-6;
-int landIdx = myIsam.addLandmark(Vector3d::Zero());
-myIsam.connectLandmark(landMarkPosFromFirstPose,landIdx,firstPoseIdx,landMarkCov);
-myIsam.connectLandmark(landMarkPosFromOrigin,landIdx,originPoseIdx,landMarkCov);
 
+//LANDMARKS
+Vector3d landMarkPosFromFirstPose;
+Vector3d landMarkPosFromOrigin;
+Matrix3d landMarkCov = Matrix3d::Identity()*1e-3;
+int landIdx; 
+
+
+//Generate Landmarks
+int i=0,NL = 5;
+while(i<NL)
+{
+    landMarkPosFromFirstPose = Vector3d(i+1.00,i+1.00,i+1.00);
+    landMarkPosFromOrigin = Vector3d(i+1.00+trueDist(0),i+1.00+trueDist(1),i+1+trueDist(2));
+    landIdx = myIsam.addLandmark(Vector3d::Zero());
+    myIsam.connectLandmark(landMarkPosFromFirstPose,landIdx,firstPoseIdx,landMarkCov);
+    myIsam.connectLandmark(landMarkPosFromOrigin,landIdx,originPoseIdx,landMarkCov);
+    i++;
+}
+
+//Optimize the Graph
 double error = myIsam.optimize(0);
 
 
-
-
-ROS_INFO("Error");
+std::cout<<"Optimization Error"<<std::endl;
 std::cout<<error<<std::endl;
-ROS_INFO("TFs");
+std::cout<<"-----------------------------------"<<std::endl;
+std::cout<<"Origin Position"<<std::endl;
 std::cout<<myIsam.getPose(0).translation()<<std::endl;
+std::cout<<"Origin Orientation"<<std::endl;
+std::cout<<myIsam.getPose(0).linear()<<std::endl;
+std::cout<<"True Origin Position"<<std::endl;
+std::cout<<Vector3d::Zero()<<std::endl;
+std::cout<<"True Origin Orientation"<<std::endl;
+std::cout<<Matrix3d::Identity()<<std::endl;
+std::cout<<"-----------------------------------"<<std::endl;
+std::cout<<"First Pose Position"<<std::endl;
 std::cout<<myIsam.getPose(1).translation()<<std::endl;
-
-
+std::cout<<"First Pose Rotation"<<std::endl;
+std::cout<<myIsam.getPose(1).linear()<<std::endl;
+std::cout<<"True First Pose Position"<<std::endl;
+std::cout<<trueDist<<std::endl;
+std::cout<<"True First Pose Rotation"<<std::endl;
+std::cout<<Matrix3d::Identity()<<std::endl;
 
 return 0;
 }
