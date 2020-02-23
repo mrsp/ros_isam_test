81d5308ba2ee398fb8673c3e34aa49eaff1f195d
Stylianos Piperakis <stpiperakis@gmail.com>
2020-02-23 18:16:19 +0200

    added 2D to 3D covariance propagation with depth measurements

---
+++ src/cov2DTo3D.h
@@ -0,0 +1,41 @@
+
+#include <eigen3/Eigen/Dense>
+#include <vector>
+#include <iostream>
+
+Eigen::MatrixXd computeCov2DTo3D(Eigen::MatrixXd cov2D, double depth, double fx, double fy, double cx, double cy, double depth_noise_cov)
+{
+
+    Eigen::MatrixXd cov3D = Eigen::MatrixXd::Zero(3,3);
+
+
+    Eigen::MatrixXd F = Eigen::MatrixXd::Zero(3,2);
+    F(0,0) = depth / fx;
+    F(1,1) = depth / fy;
+
+    Eigen::MatrixXd L = Eigen::MatrixXd::Zero(3,3);
+    L(0,0) = - cx / fx;
+    L(1,1) = - cy / fy;
+    L(2,2) = 1.00;
+
+    Eigen::MatrixXd Qz = Eigen::MatrixXd::Zero(3,3);
+    Qz(2,2) = depth_noise_cov;
+
+    cov3D.noalias() = F*cov2D*F.transpose();
+    cov3D.noalias() += L*Qz*L.transpose();
+    return cov3D;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
